25193b5f7b3af0c4a9fd2db9380f85526063c348
EO <eo@kali>
2025-11-05 20:03:54 -0400

    .

--- index.php
+++ index.php
@@ -602,14 +602,7 @@ if ($action==='new' || $action==='pagar') {
           </div>
           <input type="hidden" name="deuda_extra_actual" id="deuda_extra_actual"
                  value="<?= $deuda_extra_fmt ?>">
-
-            <div class="col-md-4">
-              <label class="form-label">texbox extra</label>
-              <input type="text" class="form-control"
-                     id="deuda_restante"
-                     value="<?= $deuda_extra_fmt ?>" disabled>
-            </div>
-
+                 
         </div></div>
       <?php else: ?>
         <input type="hidden" name="deuda_extra_actual" id="deuda_extra_actual" value="0.00">
